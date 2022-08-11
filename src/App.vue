9f21e5c05132188508a924bebe6009c0348c038f
Petrucci <Gabriel.OrlandoCampista@Honeywell.com>
2022-08-11 11:55:18 -0300

    added author

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HeaderFile from "./components/HeaderFile.vue";
 import BoardFile from "./components/BoardFile.vue";
 import "./assets/main.scss";
 </script>
-
+<!-- Autor: Gabriel Orlando Campista Petrucci -->
 <template>
   <div class="main">
     <HeaderFile></HeaderFile>
